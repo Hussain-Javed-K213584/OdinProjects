6f589e1c19759666c86db34517e56f1a67c7ec0a
Hussain <k213584@nu.edu.pk>
2023-07-22 11:32:48 +0500

    Completed the playerSelection function
    Function asks for a prompt if prompt is valid it will return the value.
    If entered value is invalid it will ask again.

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,15 +1,32 @@
 // Main function will always be called
 function main(){
     console.log("script executed");
-    let askComputer = getComputerChoice();
-    console.log(askComputer);
+    let playerChoice = playerSelection();
 }
 
+
+//Function to get player selection
+
+function playerSelection(){
+    let playerChoice;
+    while(true)
+    {
+        playerChoice = prompt("Rock, papers, or scissors?");
+        playerChoice = playerChoice.toLowerCase();
+        if (playerChoice != "rock" || playerChoice != "paper" 
+            || playerChoice != "scissors")
+        {
+            alert("Invalid choice!");
+        }
+        else{
+            return playerChoice;
+        }
+    }
+}
 
 function getComputerChoice(){
     let choices = ["rock", "paper", "scissors"];
     let randomChoice = Math.floor(Math.random() * 3); // return a random number from 0 - 3
-    console.log(randomChoice)
     return choices[randomChoice];
 }
 
