5648007f906094694fd187a18b5272b525396d9c
Muhammad Hussain Javed <k213584@nu.edu.pk>
2023-07-28 18:55:26 +0500

    Project now functions as it should
    - Added two buttons "Grid Dimension Change" and "Rainbow Effect"
    - Added functionality to sketch a black line when mouse hovers over div.
    - Used a grid layout to dynamically allocate boxes inside the grid container

--- 04-etch-a-sketch/script.js
+++ 04-etch-a-sketch/script.js
@@ -1,16 +1,57 @@
 // Create a 2D array of length 16x16
 // Push divs of size 16px and styles them with a border of 1px solid grey in css
-const GRID_CONTAINER = document.querySelector(".grid-container");
-let sketchingGrids = [];
 
-for (let i = 0; i < 256; i++)
-{
-    sketchingGrids.push(document.createElement("div"));
-    sketchingGrids[i].className = "grid-item";
-}
-// Fill in the grid container with grids
+const gridContainer = document.querySelector(".grid-container");
+const changeGridButton = document.querySelector(".set-grid");
+const DEFAULT_ROWS = 16, DEFAULT_COLS = 16;
+let rows = DEFAULT_ROWS, cols = DEFAULT_COLS;
 
-for (let i = 0; i < 256; i++)
+changeGridButton.addEventListener("click", () => {
+    let input = prompt("Input the dimensions for the grids (1-100)");
+    console.log(input);
+    if (parseInt(input) == NaN || input == null){
+        alert("Please input a valid number");
+        return;
+    }
+    rows = parseInt(input);
+    cols = rows;
+    removeOldGrids();
+    createCells(rows, cols);
+    startSketching();
+});
+
+function createCells(row, col)
 {
-    GRID_CONTAINER.appendChild(sketchingGrids[i]);
+    /*
+    Thanks to stackoverflow for this:
+    https://stackoverflow.com/questions/57550082/creating-a-16x16-grid-using-javascript
+    */
+    gridContainer.style.setProperty('--grid-rows', row);
+    gridContainer.style.setProperty('--grid-columns', cols);
+    for (let i = 0; i < (row * col); i++)
+    {
+        let cell = document.createElement("div");
+        gridContainer.appendChild(cell).className = "grid-item";
+    }
 }
+
+function startSketching()
+{
+    const gridItems = document.querySelectorAll(".grid-item");
+    // Add event listener on each item
+    gridItems.forEach(gridItem => {
+        gridItem.addEventListener('mouseover', () => {
+            gridItem.style.backgroundColor = "black";
+        })
+    })
+}
+
+function removeOldGrids(){
+    const gridItems = document.querySelectorAll(".grid-item");
+    gridItems.forEach(gridItem => {
+        gridItem.remove();
+    })
+}
+
+createCells(rows, cols);
+startSketching();
